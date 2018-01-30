c8625ba31f10b0228bc2d305a5c16bed1266b798
tebybee <tbybee@mail.csuchico.edu>
2018-01-30 11:27:41 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author Travis Bybee
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Travis. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
